dacd6dff2b61708a1e39f2ed0eec00b6b78a0cdb
ptownmannequin <52480793+ptownmannequin@users.noreply.github.com>
2019-08-27 15:59:26 -0700

    First Commit

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1566942175.5814226
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://github.com/ptownmannequin/Airship/Images/images.png',
+  defaultTitle: 'Airship',
+  defaultActionURL: 'https://ptownmannequin.github.io',
+  appKey: 'ZhPdF9PnRZ6vDA-Vvs9tVw',
+  token: 'MTpaaFBkRjlQblJaNnZEQS1WdnM5dFZ3OjVwYVZJNzFrNnZpNTRCQ2prZTRWSXFRX181ZGk3SkRoR1Z2dFV6cThqMFk',
+  vapidPublicKey: 'BJXz1UJ8KnbMaM5XsUOy5yv9Q7lrI8xxQFZSQkXUBVVvs6p3AnzN9_Yo2ykygUXf_B8d4YSHu90c8zpADtNY3R0='
+})
